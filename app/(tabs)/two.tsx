b9ed424fb5477a8ed8687c76f3e16d018898f75a
Aditi Marathe <aditimarathe1@gmail.com>
2023-06-12 18:02:41 +0530

    added cities saved in session and fetch data

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,96 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, useColorScheme } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Text, View } from "../../components/Themed";
+
+import React, { Component, useState, useEffect } from "react";
+import { TextInput, Alert } from "react-native";
+import Icon from "@expo/vector-icons/FontAwesome";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
 
 export default function TabTwoScreen() {
+  const [search, setSearch] = useState("");
+  const [cities, setCities] = useState<any>({});
+  const [city, setCity] = useState<any>(null);
+  const API_KEY = "BvGGLMWeaet2tFqE7OAiKw==tG6UxN6s8a8jOpWy";
+  const colorScheme = useColorScheme();
+  const _color = colorScheme === "dark" ? "white" : "black";
+  const router = useRouter();
+  useEffect(() => {
+    async function getData() {
+      const _cities = JSON.parse(
+        (await AsyncStorage.getItem("cities")) || "{}"
+      );
+      console.dir("cities:", _cities);
+      setCities(_cities);
+    }
+    getData();
+  }, []);
+  const featchCity = async () => {
+    console.dir(cities);
+    if (cities[search]) {
+      setCity(cities[search]);
+      return;
+    }
+    const url = "https://api.api-ninjas.com/v1/city?name=" + search;
+    try {
+      const req = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Api-Key": API_KEY,
+        },
+      });
+      const _cities = await req.json();
+
+      if (_cities.length > 0) {
+        let city = _cities[0];
+        city.name = search;
+        setCity(city);
+        cities[search] = city;
+      }
+      setCities(cities);
+      await AsyncStorage.setItem("cities", JSON.stringify(cities));
+      //console.log(JSON.stringify(cities))
+    } catch (error) {
+      setCity(null);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab </Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/cities.tsx" />
+      <View style={styles.enterCityColumnRow}>
+        <View style={styles.enterCityColumn}>
+          <Text>City Finder</Text>
+          <TextInput
+            value={search}
+            placeholder="Enter City Name"
+            style={[styles.enterCity, { color: _color, borderColor: _color }]}
+            onChangeText={(newText) => {
+              setSearch(newText);
+            }}
+          ></TextInput>
+          <Icon
+            name="search"
+            style={styles.icon}
+            onPress={() => {
+              featchCity();
+            }}
+          ></Icon>
+          <Text>{Object.keys(cities).length}</Text>
+        </View>
+      </View>
+      <View style={styles.enterCityColumnRow1}>
+        <FlatList
+          data={Object.values(cities)}
+          renderItem={(item: any) => {
+            return <Text style={styles.item} onPress={() => {
+            //  console.log(item.item)
+              item.item.lat = item.item.latitude;
+              item.item.long = item.item.longitude;
+              router.replace({pathname: "/(tabs)/" , params: item.item })
+            }}>{item.item.name}</Text>;
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -16,16 +98,38 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "baseline",
+    justifyContent: "flex-start",
   },
-  title: {
+  item: {
+    padding: 10,
     fontSize: 20,
-    fontWeight: 'bold',
+    height: 50
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  enterCity: {
+    height: 54,
+    width: 350,
+    borderWidth: 1,
+    paddingLeft: 5,
+  },
+  icon: {
+    color: "rgba(128,128,128,1)",
+    fontSize: 40,
+    marginTop: 30,
+    marginLeft: 150,
+  },
+  enterCityColumn: {
+    width: 290,
+    marginBottom: 10,
+  },
+  enterCityColumnRow: {
+    height: 134,
+    flexDirection: "row",
+    marginLeft: 39,
+  },
+  enterCityColumnRow1: {
+    flexDirection: "row",
+    marginLeft: 39,
+    marginTop: 10,
   },
 });
